4fb8a2c4ab1319ffb19dec46ba8380934793b8d3
Ayyappa Kammila <68323119+ayyappamani@users.noreply.github.com>
2020-11-18 07:53:12 +0530

    Update app-routing.module.ts

--- app-routing.module.ts
+++ app-routing.module.ts
@@ -1,18 +1,5 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import {LoginComponent} from './login/login.component';
-import  {UserDashboardComponent} from './user-dashboard/user-dashboard.component';
-import {RegisterComponent} from './register/register.component';
-import {RecruiterDashboardComponent} from './recruiter-dashboard/recruiter-dashboard.component';
-const routes: Routes = [
-  { path: '',component:LoginComponent },
-  { path: 'register',component:RegisterComponent },
-  { path: 'user-dashboard/:id',component:UserDashboardComponent },
-  { path: 'login',component:LoginComponent }
-];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
